165537cbbcc0c1cf49bd7f6a7ba5666f1c826c8e
SnareChops <snare_chops@yahoo.com>
2015-07-13 12:28:19 -0400

    Initial commit

---
+++ typings/tsd.d.ts
@@ -0,0 +1,5 @@
+
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="angularfire/angularfire.d.ts" />
+/// <reference path="firebase/firebase.d.ts" />
